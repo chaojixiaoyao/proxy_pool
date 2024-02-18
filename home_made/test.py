4ea9dfc7d6dff51c40143162053836f9f4cfd84a
chaojixiaoyao <1361720765@qq.com>
2024-02-18 20:34:28 +0800

    '8'

--- home_made/test.py
+++ home_made/test.py
@@ -1,4 +1,5 @@
 import redis
+import json
 
 # Redis连接参数
 redis_config = {"host": "127.0.0.1", "port": 6379, "password": "", "db": 0}
@@ -8,5 +9,8 @@ r = redis.Redis(**redis_config)
 
 # 获取哈希表中键值对的数量
 item_dict = r.hgetall("use_proxy")
+
+# 转换item为Python字典
 for item in item_dict.values():
-    print(item)
+    item_dict = json.loads(item)
+    print(item_dict)
