a56b6ce844ab29c09237968e7d108fe7b9e68ced
chaojixiaoyao <1361720765@qq.com>
2024-02-18 16:59:50 +0800

    '4'

--- home_made/test.py
+++ home_made/test.py
@@ -6,6 +6,6 @@ redis_config = {"host": "127.0.0.1", "port": 6379, "password": "", "db": 0}
 # 连接到Redis
 r = redis.Redis(**redis_config)
 
-# 获取数据量
-key_type = r.type("use_proxy")
-print("Type of 'use_proxy' key in Redis:", key_type)
+# 获取哈希表中键值对的数量
+item_dict = r.hgetall("use_proxy")
+print(item_dict.values())
