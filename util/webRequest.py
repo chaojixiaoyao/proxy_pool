f2460e5a985f27c8c3590c0e72da56cc417ab5b4
chaojixiaoyao <1361720765@qq.com>
2024-02-18 15:12:26 +0800

    useragent

--- util/webRequest.py
+++ util/webRequest.py
@@ -38,6 +38,9 @@ class WebRequest(object):
         """
         ua_list = [
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.2277.112",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0",
         ]
         return random.choice(ua_list)
 
