0795592243df9d9041b444206cd3b7a36b3476b0
chaojixiaoyao <1361720765@qq.com>
2024-02-18 16:52:44 +0800

    '2'

--- home_made/test.py
+++ home_made/test.py
@@ -7,5 +7,5 @@ redis_config = {"host": "127.0.0.1", "port": 6379, "password": "", "db": 0}
 r = redis.Redis(**redis_config)
 
 # 获取数据量
-data_size = r.dbsize()
-print("Total number of keys in the Redis database:", data_size)
+value = r.get("use_proxy")
+print("Value of 'use_proxy' key in Redis:", value)
