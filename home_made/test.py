2195b29fa7df02e778a5292e9dad970e1da2f267
chaojixiaoyao <1361720765@qq.com>
2024-02-18 21:52:32 +0800

    '10'

--- home_made/test.py
+++ home_made/test.py
@@ -28,27 +28,29 @@ urls = [
     'https://webcache.googleusercontent.com/search?q=cache:https://play.google.com/store/apps/details?id=com.antonio.ukrainiantv',
 ]
 
-proxies = list(filter(lambda x: json.loads(x).get("https"), item_dict.values()))
-for item in proxies:
-    print(item)
+# proxies = list(filter(lambda x: json.loads(x).get("https"), item_dict.values()))
+# for item in proxies:
+#     print(item)
 
 # 转换item为Python字典
-# for num, item in enumerate(item_dict.values(), start=1):
-#     item_dict = json.loads(item)
-#     print("proxy", item_dict)
-#
-#     proxy = item_dict['proxy']
-#     proxies = {
-#         'http': f'http://{proxy}',
-#         'https': f'https://{proxy}'
-#     }
-#
-#     url = urls.pop()
-#     response = requests.get(url, headers=headers, proxies=proxies)
-#     http_code = response.status_code
-#     xp = etree.HTML(response.text)
-#     print('title', xp.xpath('//title/text()'))
-#     print('http_code', http_code)
-#
-#     if num == 10:
-#         break
+for num, item in enumerate(item_dict.values(), start=1):
+    item_dict = json.loads(item)
+    if not item_dict.get("https"):
+        continue
+    print("proxy", item_dict)
+
+    proxy = item_dict['proxy']
+    proxies = {
+        'http': f'http://{proxy}',
+        'https': f'https://{proxy}'
+    }
+
+    url = urls.pop()
+    response = requests.get(url, headers=headers, proxies=proxies)
+    http_code = response.status_code
+    xp = etree.HTML(response.text)
+    print('title', xp.xpath('//title/text()'))
+    print('http_code', http_code)
+
+    if num == 10:
+        break
