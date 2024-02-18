bf13c828ef8a8d5ee237bc500464296ffc7e0dd1
chaojixiaoyao <1361720765@qq.com>
2024-02-18 21:35:44 +0800

    '9'

--- home_made/test.py
+++ home_made/test.py
@@ -1,5 +1,8 @@
 import redis
 import json
+import requests
+from lxml import etree
+
 
 # Redis连接参数
 redis_config = {"host": "127.0.0.1", "port": 6379, "password": "", "db": 0}
@@ -10,7 +13,38 @@ r = redis.Redis(**redis_config)
 # 获取哈希表中键值对的数量
 item_dict = r.hgetall("use_proxy")
 
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'}
+urls = [
+    'https://webcache.googleusercontent.com/search?q=cache:https://play.google.com/store/apps/details?id=com.antonio.persiantv',
+    'https://webcache.googleusercontent.com/search?q=cache:https://play.google.com/store/apps/details?id=com.antonio.ruradio',
+    'https://webcache.googleusercontent.com/search?q=cache:https://play.google.com/store/apps/details?id=com.antonio.latintv',
+    'https://webcache.googleusercontent.com/search?q=cache:https://play.google.com/store/apps/details?id=com.antonio.ngradio',
+    'https://webcache.googleusercontent.com/search?q=cache:https://play.google.com/store/apps/details?id=com.antonio.sradio',
+    'https://webcache.googleusercontent.com/search?q=cache:https://play.google.com/store/apps/details?id=com.antonio.radio',
+    'https://webcache.googleusercontent.com/search?q=cache:https://play.google.com/store/apps/details?id=com.antonio.bihradio',
+    'https://webcache.googleusercontent.com/search?q=cache:https://play.google.com/store/apps/details?id=com.antonio.bulgariantv',
+    'https://webcache.googleusercontent.com/search?q=cache:https://play.google.com/store/apps/details?id=com.antonio.latviantv',
+    'https://webcache.googleusercontent.com/search?q=cache:https://play.google.com/store/apps/details?id=com.antonio.ukrainiantv',
+]
+
 # 转换item为Python字典
-for item in item_dict.values():
+for num, item in enumerate(item_dict.values(), start=1):
     item_dict = json.loads(item)
-    print(item_dict)
+    print("proxy", item_dict)
+
+    proxy = item_dict['proxy']
+    proxies = {
+        'http': f'http://{proxy}',
+        'https': f'https://{proxy}'
+    }
+
+    url = urls.pop()
+    response = requests.get(url, headers=headers, proxies=proxies)
+    http_code = response.status_code
+    xp = etree.HTML(response.text)
+    print('title', xp.xpath('//title/text()'))
+    print('http_code', http_code)
+
+    if num == 10:
+        break
